4bbc76364dd04fffe0f52b5438a7c2e9a95e987b
Riccardo Miccini <rimiccini@yahoo.it>
2021-03-12 21:06:31 +0100

    more progresses

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,9 @@
 // Empty JS for your own code to be here
 $('.navbar-nav>li>a').on('click', function(){
+    $('.sidebar-overlay').css('left','200vh');
     $('.sidebar-overlay').collapse('hide');
+});
+
+$('button.sidebar-toggler').on('click', function(){
+    $('.sidebar-overlay').css('left','0');
 });
